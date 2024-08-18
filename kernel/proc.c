600316153cd3ec2cf7c6081a98656c05c342203b
Guaaaava <641331276@qq.com>
2024-08-18 21:49:13 +0800

    完成实验syscall: trace;

--- kernel/proc.c
+++ kernel/proc.c
@@ -164,6 +164,9 @@ freeproc(struct proc *p)
   p->killed = 0;
   p->xstate = 0;
   p->state = UNUSED;
+
+  // variable for trace
+  p->trace_mask = 0;
 }
 
 // Create a user page table for a given process,
@@ -294,6 +297,9 @@ fork(void)
 
   // Cause fork to return 0 in the child.
   np->trapframe->a0 = 0;
+
+  // copy trace_mask in child
+  np->trace_mask = p->trace_mask;
 
   // increment reference counts on open file descriptors.
   for(i = 0; i < NOFILE; i++)
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -95,3 +95,17 @@ sys_uptime(void)
   release(&tickslock);
   return xticks;
 }
+
+// Add a sys_trace() function in kernel/sysproc.c
+uint64
+sys_trace(void)
+{
+  int mask;
+  if (argint(0, &mask) < 0)
+    return -1;
+
+  struct proc *p = myproc();
+  p->trace_mask = mask;
+
+  return 0;
+}

5f211e4ae6a7b388366481a94c9e9d2fdcea5800
Guaaaava <641331276@qq.com>
2024-08-23 22:42:44 +0800

    完成traps实验: RISC-V assembly;

--- user/call.c
+++ user/call.c
@@ -12,6 +12,9 @@ int f(int x) {
 }
 
 void main(void) {
-  printf("%d %d\n", f(8)+1, 13);
+//  printf("%d %d\n", f(8)+1, 13);
+  unsigned int i = 0x00646c72; // for little-endian
+//  unsigned int i = 0x726c6400; for big-endian
+	printf("H%x Wo%s\n", 57616, &i);
   exit(0);
 }

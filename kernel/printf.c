35b340d6306e41fcdb70642d69c871f1aca4a29e
Guaaaava <641331276@qq.com>
2024-08-24 11:59:02 +0800

    完成traps实验: RISC-V assembly; backtrace;

--- kernel/printf.c
+++ kernel/printf.c
@@ -122,6 +122,9 @@ panic(char *s)
   printf(s);
   printf("\n");
   panicked = 1; // freeze uart output from other CPUs
+  
+  backtrace();
+
   for(;;)
     ;
 }
@@ -131,4 +134,21 @@ printfinit(void)
 {
   initlock(&pr.lock, "pr");
   pr.locking = 1;
+}
+
+void
+backtrace()
+{
+  printf("backtrace:\n");
+
+  uint64 fp = r_fp();
+  uint64 *frame = (uint64 *)fp;
+
+  uint64 up = PGROUNDUP(fp);
+  uint64 down = PGROUNDDOWN(fp);
+  while (fp < up && fp > down) {
+    printf("%p\n", *(frame - 1));
+    fp = *(frame - 2);
+    frame = (uint64 *)fp;
+  }
 }
--- kernel/sysproc.c
+++ kernel/sysproc.c
@@ -70,6 +70,9 @@ sys_sleep(void)
     sleep(&ticks, &tickslock);
   }
   release(&tickslock);
+
+  backtrace(); // backtrace in Lab 4
+
   return 0;
 }
 

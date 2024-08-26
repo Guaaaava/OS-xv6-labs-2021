cbe159b5eaec7d4c1cba8cf4d2753b93ce92016a
Guaaaava <641331276@qq.com>
2024-08-26 21:45:44 +0800

    完成thread实验: switch; using thread;

--- notxv6/ph.c
+++ notxv6/ph.c
@@ -17,6 +17,8 @@ struct entry *table[NBUCKET];
 int keys[NKEYS];
 int nthread = 1;
 
+pthread_mutex_t locks[NBUCKET];
+
 
 double
 now()
@@ -41,6 +43,8 @@ void put(int key, int value)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&locks[i]);
+
   // is the key already present?
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
@@ -55,6 +59,7 @@ void put(int key, int value)
     insert(key, value, &table[i], table[i]);
   }
 
+  pthread_mutex_unlock(&locks[i]);
 }
 
 static struct entry*
@@ -62,11 +67,15 @@ get(int key)
 {
   int i = key % NBUCKET;
 
+  pthread_mutex_lock(&locks[i]);
+
 
   struct entry *e = 0;
   for (e = table[i]; e != 0; e = e->next) {
     if (e->key == key) break;
   }
+
+  pthread_mutex_unlock(&locks[i]);
 
   return e;
 }
@@ -104,6 +113,10 @@ main(int argc, char *argv[])
   pthread_t *tha;
   void *value;
   double t1, t0;
+
+  for (int i = 0; i < NBUCKET; i ++) {
+    pthread_mutex_init(&locks[i], NULL);
+  }
 
 
   if (argc < 2) {

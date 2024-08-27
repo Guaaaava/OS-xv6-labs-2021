9f874cbc20d46793ef289a64619e3d71f76f250b
Guaaaava <641331276@qq.com>
2024-08-27 15:01:34 +0800

    完成lock实验: memory allocator;

--- kernel/param.h
+++ kernel/param.h
@@ -11,3 +11,5 @@
 #define NBUF         (MAXOPBLOCKS*3)  // size of disk block cache
 #define FSSIZE       1000  // size of file system in blocks
 #define MAXPATH      128   // maximum file path name
+
+#define LOCK_NAME_LEN 16  // max len of lock name
